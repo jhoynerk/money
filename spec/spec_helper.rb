b05cef3640f9bbf83baee89d2904853baf89b2dd
Jhoynerk Caraballo <jhoynerk@gmail.com>
2017-08-07 13:08:26 -0300

    Initializer configure to money class, to rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,14 @@
 require "bundler/setup"
 require "money"
 
+Money.configure do |config|
+  config.default_currency = "EUR"
+  config.conversions = {
+    'USD' => 1.11,
+    'Bitcoin' => 0.0047
+  }
+end
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
